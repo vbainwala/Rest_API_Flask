d579e8e21c71d800d5d4eb8444c51c2d7af0e671
vbainwala <var1809@gmail.com>
2022-01-31 21:30:32 +0530

    Added Postgres and psycopg2 changes

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,7 +11,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://pvoximlfppsbaa:618f39d0ed0f264d42120007be1c884c658b82ade223310d3b153547dbfd1746@ec2-54-157-15-228.compute-1.amazonaws.com:5432/d83f1r1i8se68d','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'vareesh'
 api = Api(app)
