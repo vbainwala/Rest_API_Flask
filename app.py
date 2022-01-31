73e90bf98b20e311c3020da9d630c1772fd8c3e6
vbainwala <var1809@gmail.com>
2022-01-31 21:47:40 +0530

    Removing Local SQLite Connection URI for testing Postgres

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri,'sqlite:///data.db')
+#'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'vareesh'
 api = Api(app)
